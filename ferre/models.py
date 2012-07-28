2f6331984d034775171bbc9c28dfd172d3ede62b
Martin Onetti <onetti.martin@gmail.com>
2012-07-28 20:44:00 -0300

    cambios en el modelo

--- ferre/models.py
+++ ferre/models.py
@@ -1,40 +1,58 @@
 from django.db import models
 
 class Articulo (models.Model):
-	cod_art = models.SlugField()
-	descripcion = models.TextField()
-	imagen = models.ImageField(upload_to='uploads')
+    cod_art = models.SlugField()
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='uploads')
+    marca = models.ForeignKey('Fabricante',null=True,blank=True)
+    material = models.ForeignKey('Material',null=True,blank=True)
+    medida = models.ForeignKey('Medida',null=True,blank=True)
+
+class Material(models.Model):
+    name = models.CharField(max_length=255)
+
+class Medida(models.Model):
+    name = models.CharField(max_length=255)
 
 class Ferreteria (models.Model):
-	cod_fer = models.SlugField()
-	nombre = models.CharField(max_length=255)
-	domicilio = models.CharField(max_length=255)
-	telefono = models.CharField(max_length=255)
-	email = models.EmailField()
+    cod_fer = models.SlugField()
+    nombre = models.CharField(max_length=255)
+    domicilio = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=255)
+    email = models.EmailField()
 
 class Proveedor (models.Model):
-	cod_prov = models.SlugField()
-	nombre = models.CharField(max_length=255)
-	domicilio = models.CharField(max_length=255)
-	telefono = models.CharField(max_length=255)
-	email = models.EmailField()
+    cod_prov = models.SlugField()
+    nombre = models.CharField(max_length=255)
+    domicilio = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=255)
+    email = models.EmailField()
 
 class Fabricante (models.Model):
-	cod_fab = models.SlugField()
-	nombre = models.CharField(max_length=255)
+    cod_fab = models.SlugField()
+    nombre = models.CharField(max_length=255)
 
 class ArticuloFerreteria (models.Model):
-	articulo = models.ForeignKey('Articulo')
-	ferreteria = models.ForeignKey('Ferreteria')
-	precio = models.FloatField()
-	stock = models.SmallIntegerField(default=0)
+    articulo = models.ForeignKey('Articulo')
+    ferreteria = models.ForeignKey('Ferreteria')
+    stock = models.SmallIntegerField(default=0)
+    proveedor = models.ManyToManyField('Proveedor')
+
+class ArticuloFerreteriaPublico(models.Model):
+    art_ferre = models.ForeignKey('ArticuloFerreteria')
+    precio = models.FloatField()
+    unidad = models.ForeignKey('Unidad',null=True,blank=True)
 
 class CatalogoProveedor (models.Model):
-	cod_cat = models.SlugField()
-	articulo = models.ForeignKey('Articulo')
-	proveedor = models.ForeignKey('Proveedor')
-	descripcion = models.TextField()
-	imagen = models.ImageField(upload_to='uploads')
-	precio = models.FloatField()
-	fabricante = models.ForeignKey('Fabricante')
+    cod_cat = models.SlugField()
+    articulo = models.ForeignKey('Articulo')
+    proveedor = models.ForeignKey('Proveedor')
+    descripcion = models.TextField()
+    precio = models.FloatField()
+    fecha = models.DateField()
+    unidad = models.ForeignKey('Unidad',null=True,blank=True)
+
+class Unidad(models.Model):
+    name = models.CharField(max_length=255)
+    
 
