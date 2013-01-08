f5b50cb68c8eeb9f33b64ed574770df92bb7daed
Martin Onetti <onetti.martin@gmail.com>
2013-01-08 09:15:02 -0300

    varios cambios. se agregaron templates para la ABM de proveedores

--- ferre/views.py
+++ ferre/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from models import Proveedor, Articulo
+from forms import ProveedorNuevoForm
+import datetime
 
 def home(request):
-	return render(
+    now = datetime.datetime.now()
+    articles = Articulo.objects.all().order_by('descripcion')
+
+    return render(
 		request,
 		'index.html',
-		{}
+        {
+        'current_time':now,
+        'articles':articles,
+        }
 	)
+
+def providers_manager(request):
+    now = datetime.datetime.now()
+    providers = Proveedor.objects.all().order_by('nombre')
+    return render(
+		request,
+		'manager_proveedor.html',
+		{
+        'providers':providers,
+        'current_time':now,
+        }
+	)
+
+def providers_new(request):
+    if request.method == "POST":
+        form = ProveedorNuevoForm(request.POST)
+        if form.is_valid():
+            obj = form.save()
+            return redirect("/providers/new")
+    form = ProveedorNuevoForm()
+    return render(
+        request,
+        'proveedor_nuevo.html',
+        {
+            'form':form,     
+        }
+    )
+
+def providers_edit(request, id):
+    provider = Proveedor.objects.get(pk=id)
+    form = ProveedorNuevoForm(instance=provider)
+    import pdb; pdb.set_trace()
+    if form.is_valid():
+        form.save()
+    return render(
+        request,
+        'proveedor_detalle.html',
+        {
+            'form':form,     
+        }
+    )
+    
--- sifre/urls.py
+++ sifre/urls.py
@@ -8,14 +8,16 @@ from ferre import api
 from ferre.api import FerreteriaResource, ArticuloFerreteriaResource, ProveedorResource
 
 
-api = Api(api_name = 'v1')
+api = Api(api_name = 'api_rest')
 api.register(ProveedorResource())
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'ferre.views.home', name='home'),
+    url(r'^providers/manager', 'ferre.views.providers_manager'),
+    url(r'^providers/new', 'ferre.views.providers_new'),
+    url(r'^providers/(?P<id>\d+)', 'ferre.views.providers_edit'),
 )
 
 urlpatterns += patterns('',
@@ -31,4 +33,5 @@ urlpatterns += patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 )
